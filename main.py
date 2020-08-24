10b0728f4528e0c5bffede725739a9713e74d569
Sebastian Henneman <seb.henman@gmail.com>
2020-08-24 20:01:11 +0200

    now draw cell color on init

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class App:
         self._running = True
         self._display_surf.fill(white)
         self.Grid.draw(self._display_surf)
+        self.Grid.draw_cells(self._display_surf)
 
         pygame.display.set_caption("Descent Line of Sight Checker!")
 
