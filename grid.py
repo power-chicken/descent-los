5bbdf70b0cc2ca33b67cc92265eed30aaaa1adba
Sebastian Henneman <seb.henman@gmail.com>
2020-08-24 18:55:01 +0200

    Added gitignore, draw squares, enable mouseclick input

--- grid.py
+++ grid.py
@@ -1,1 +1,20 @@
+import pygame
+from pygame.locals import *
 
+
+class Grid:
+
+    def __init__(self):
+        self._n_rows, self._n_columns = 10, 10
+        self._square_size = 40
+        self._square_color = (255, 0, 0)
+
+    def draw(self, pygame_display_surf):
+
+        for i_row in range(self._n_rows):
+            for i_col in range(self._n_columns):
+                pygame.draw.rect(pygame_display_surf, self._square_color,
+                                 (i_row * self._square_size,
+                                  i_col * self._square_size,
+                                  self._square_size,
+                                  self._square_size), 2)
--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 import pygame
 from pygame.locals import *
+from grid import *
+
+white = (255, 255, 255)
+red = (255, 0, 0)
 
 
 class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 640, 400
+        self.size = self.weight, self.height = 640, 500
+        self.Grid = None
+        self.clock = pygame.time.Clock()
+        self._fps = 3
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.Grid = Grid()
         self._running = True
+        self._display_surf.fill(white)
+        self.Grid.draw(self._display_surf)
+
+        pygame.display.set_caption("Descent Line of Sight Checker!")
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                self.on_lbutton_down(event)
+            elif event.button == 2:
+                self.on_mbutton_down(event)
+            elif event.button == 3:
+                self.on_rbutton_down(event)
+
+    def on_lbutton_down(self, event):
+        print("l")
+    def on_mbutton_down(self, event):
+        print("m")
+    def on_rbutton_down(self, event):
+        print("r")
 
     def on_loop(self):
-        pass
+        self.clock.tick(self._fps)
 
     def on_render(self):
-        pass
+        pygame.display.flip()
 
     def on_cleanup(self):
         pygame.quit()
@@ -30,7 +56,7 @@ class App:
         if self.on_init() == False:
             self._running = False
 
-        while (self._running):
+        while self._running:
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
