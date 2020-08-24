b47d5e57b4c76af0c11fa4bd68ccc8279290437d
Sebastian Henneman <seb.henman@gmail.com>
2020-08-24 22:06:46 +0200

    added some text to show button functions

--- main.py
+++ main.py
@@ -2,29 +2,50 @@ import pygame
 from pygame.locals import *
 from grid import *
 
-white = (255, 255, 255)
-red = (255, 0, 0)
+
+rgb_red = (255, 0, 0)
+rgb_black = (0, 0, 0)
+rgb_blue = (0, 0, 255)
+rgb_white = (255, 255, 255)
+rgb_green = (0, 255, 0)
+rgb_gray = (100, 100, 100)
 
 
 class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 640, 500
+        self.size = self.width, self.height = 800, 500
         self.Grid = None
         self.clock = pygame.time.Clock()
         self._fps = 15
+
+        self._system_font = None
+
+        self._text_hero_surface = None
+        self._text_monster_surface = None
+        self._text_obstacle_surface = None
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.Grid = Grid()
         self._running = True
-        self._display_surf.fill(white)
+        self._display_surf.fill(rgb_white)
         self.Grid.draw_grid_lines(self._display_surf)
         self.Grid.draw_cells(self._display_surf)
 
         pygame.display.set_caption("Descent Line of Sight Checker!")
+
+        self._system_font = pygame.font.SysFont("comicsansms", size=16)
+        self._text_hero_surface = self._system_font.render("Hero position (left mouse button)", True, rgb_blue)
+        self._display_surf.blit(self._text_hero_surface, (self.width - 300, 100))
+
+        self._text_monster_surface = self._system_font.render("Monster position (middle mouse button)", True, rgb_red)
+        self._display_surf.blit(self._text_monster_surface, (self.width - 300, 150))
+
+        self._text_obstacle_surface = self._system_font.render("Obstacle position (right mouse button)", True, rgb_green)
+        self._display_surf.blit(self._text_obstacle_surface, (self.width - 300, 200))
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
