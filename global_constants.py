dc4d1df353b9766dfefd58c621db99ba8d9ec68e
Sebastian Henneman <seb.henman@gmail.com>
2020-08-30 12:53:54 +0200

    added some default colors

--- global_constants.py
+++ global_constants.py
@@ -1,7 +1,13 @@
 rgb_red = (255, 0, 0)
-rgb_black = (0, 0, 0)
 rgb_blue = (0, 0, 255)
-rgb_white = (255, 255, 255)
 rgb_green = (0, 255, 0)
-rgb_gray = (100, 100, 100)
 rgb_yellow = (255, 255, 0)
+rgb_orange = (255, 165, 0)
+rgb_purple = (128, 0, 128)
+
+rgb_white = (255, 255, 255)
+rgb_gray = (100, 100, 100)
+rgb_brown = (165, 42, 42)
+rgb_black = (0, 0, 0)
+
+
